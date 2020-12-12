9222e4d3785b18ded3936889f880a1adeff3b0e5
表川哲也 <omotegawatetuya@omotegawa.local>
2020-12-12 15:13:48 +0900

    授業でしたこと

---
+++ Study4.java
@@ -0,0 +1,23 @@
+package javastudy;
+
+public class Study4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int number= 1 ; number <= 9 ; number ++) {
+			for(int i = 1 ; i <= 9 ; i ++) {
+				System.out.print(i * number + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		for(int m= 1 ; m <= 9 ; m ++) {
+			
+		}
+	}
+	
+	
+	
+
+}
