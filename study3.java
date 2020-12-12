61a475289c3d2a526cff73db195f2e6071ff18d7
表川哲也 <omotegawatetuya@omotegawa.local>
2020-12-12 15:13:26 +0900

    授業でしたこと

---
+++ study3.java
@@ -0,0 +1,128 @@
+package javastudy;
+
+public class study3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 1 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 2 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 3 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 4 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 5 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 6 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 7 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 8 * number + " ");
+		}
+		System.out.println();
+		for (int number = 1 ; number <= 9 ; number ++) {
+			System.out.print( 9 * number + " ");
+		}
+		System.out.println();
+		
+		for(int number1 = 1 ; number1 <= 4 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.print("■" + " ");
+		for(int number1 = 1 ; number1 <= 4 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 3 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 3 ; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 3 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 2 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 5 ; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 2 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 1 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 7; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 1 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 9; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 1 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 7; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 1 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 2 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 5; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 2 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 3 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 3; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 3 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+		for(int number1 = 1 ; number1 <= 4 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 1; number1 ++) {
+			System.out.print("■" + " ");
+		}
+		for(int number1 = 1 ; number1 <= 4 ; number1 ++) {
+			System.out.print("□" + " ");
+		}
+		System.out.println();
+	}
+
+}
